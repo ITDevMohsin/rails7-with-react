bce4e3cffd4800a218453698014fffe8c1eb542a
Muhammad Mohsin Saeed <21135288+ITDevMohsin@users.noreply.github.com>
2024-03-28 19:14:51 -0400

    converting functional component to class component

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -2,14 +2,16 @@ import * as React from "react";
 import * as ReactDOM from 'react-dom/client';
 import QuestionList from "./QuestionList";
 
-const Welcome = () => {
-    return (
-        <div className="container">
-            <h1>Hello World! Welcome to Rails 7 with React JS</h1>
-            <p className="lead">This is the first lecture and there are many more coming</p>
-            <QuestionList/>
-        </div>
-    )
+class Welcome extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Hello World! Welcome to Rails 7 with React JS</h1>
+                <p className="lead">This is the first lecture and there are many more coming</p>
+                <QuestionList/>
+            </div>
+        )
+    }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('welcome'))
