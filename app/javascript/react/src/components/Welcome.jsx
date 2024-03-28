20ebaabb03fa72fe6e1698993cd5b5ac21529f95
Muhammad Mohsin Saeed <21135288+ITDevMohsin@users.noreply.github.com>
2024-03-28 19:16:00 -0400

    reverting back from class component to functional component

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -2,22 +2,16 @@ import * as React from "react";
 import * as ReactDOM from 'react-dom/client';
 import QuestionList from "./QuestionList";
 
-class Welcome extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <h1>Hello World! Welcome to Rails 7 with React JS</h1>
-                <p className="lead">This is the first lecture and there are many more coming</p>
-                <QuestionList/>
-            </div>
-        )
-    }
+const Welcome = () => {
+    return (<div className="container">
+            <h1>Hello World! Welcome to Rails 7 with React JS</h1>
+            <p className="lead">This is the first lecture and there are many more coming</p>
+            <QuestionList/>
+        </div>)
 }
 
 const root = ReactDOM.createRoot(document.getElementById('welcome'))
-root.render(
-    <React.StrictMode>
-        <Welcome/>
-    </React.StrictMode>
-)
+root.render(<React.StrictMode>
+    <Welcome/>
+</React.StrictMode>)
 export default Welcome;
