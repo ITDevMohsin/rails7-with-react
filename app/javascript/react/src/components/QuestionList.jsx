2962d20a0c226305b556d3462155be3c4a4f48ca
Muhammad Mohsin Saeed <21135288+ITDevMohsin@users.noreply.github.com>
2024-03-28 22:21:15 -0400

    fetching API response in questionlist component

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,65 +1,30 @@
 import * as React from "react";
+import {useState, useEffect} from "react";
 import * as ReactDOM from 'react-dom';
 import QuestionDetail from "./QuestionDetail";
 
 const QuestionList = () => {
-    const questionList = [
-        {
-            id: 1,
-            title: 'How to check if a key is present in a Hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 2,
-            title: 'What is the difference between string and symbol?',
-            tag: 'Ruby'
-        },
-        {
-            id: 3,
-            title: 'What happened if you add two same keys in Hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 4,
-            title: 'How to delete a given key from Hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 5,
-            title: 'How to check if 2 hashes are identical?',
-            tag: 'Ruby'
-        },
-        {
-            id: 6,
-            title: 'How to combine 2 hashes in Ruby?',
-            tag: 'Ruby'
-        },
-        {
-            id: 7,
-            title: 'How to get unique keys from 2 hashes in Ruby?',
-            tag: 'Ruby'
-        },
-        {
-            id: 8,
-            title: 'What does the has_key?, key?, member? and include? methods in a Hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 9,
-            title: 'What are blocks in Ruby?',
-            tag: 'Ruby'
-        },
-        {
-            id: 10,
-            title: 'Does the order of keys matter to compare 2 hashes in Ruby?',
-            tag: 'Ruby'
-        }
-    ]
+
+    const [questionsList, setQuestionsList] = useState([])
+    const questionsURL = 'http://localhost:3000/api/v1/questions'
+
+    const fetchQuestionList = () => {
+        fetch(questionsURL)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setQuestionsList(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchQuestionList()
+    }, [])
 
     return (
         <div className="row">
             <div className="col-lg-10 mx-auto">
-                {questionList.map((question) =>
+                {questionsList.map((question) =>
                     <QuestionDetail question={question} key={question.id}/>
                 )}
             </div>
